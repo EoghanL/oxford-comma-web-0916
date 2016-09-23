02a015aad31751fab89e6f9e22cee0b5898b592c
Eoghan Leddy <eoghan.leddy@flatironschool.com>
2016-09-23 19:12:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  fin_str = String.new
+  while(array.length > 3)
+    fin_str += "#{array.shift}, "
+  end
+  if(array.length == 3)
+    fin_str += "#{array.shift}, "
+    fin_str += array.join(", and ")
+    array.shift(2)
+    fin_str
+  end
+  fin_str += array.join(" and ") if array.length == 2
+  fin_str = array[0] if array.length == 1
+  fin_str
 end
